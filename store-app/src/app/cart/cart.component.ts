80e0416abe21a99120ed1c288163f16db940b40c
azizx018 <chiara.aziz@gmail.com>
2021-10-17 22:12:20 -0500

    cart service added

--- store-app/src/app/cart/cart.component.ts
+++ store-app/src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../models/products';
+
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  productList: Product[] = [];
 
-  constructor() { }
+  constructor(private cartService: CartService) {
+
+  }
 
   ngOnInit(): void {
+    this.productList = this.cartService.getCart();
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart();
+    this.productList = [];
+    alert("Cart has been cleared.");
   }
 
 }
