6a12f611b032dc2b0e68bd6eef2bd55f454d4b72
azizx018 <chiara.aziz@gmail.com>
2021-10-20 21:18:56 -0500

    updated view and layout

--- store-app/src/app/product.service.ts
+++ store-app/src/app/product.service.ts
@@ -17,19 +17,4 @@ export class ProductService {
 
 }
 
-// [
-//   {
-//     id: 1,
-//     imageUrl: 'https://images.pexels.com/photos/2905238/pexels-photo-2905238.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=150',
-//     productName: 'Backpack',
-//     price: 7,
-//     quantity: 1
-//   },
-//   {
-//     id: 2,
-//     imageUrl: 'https://images.pexels.com/photos/251454/pexels-photo-251454.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=150',
-//     productName: 'Socks',
-//     price: 3,
-//     quantity: 1
-//   }
-// ];
+
